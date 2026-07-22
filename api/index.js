5bb66444b21be880b3b1f39e1d6ee317992ae59b
Rahul84825 <activegamer789@gmail.com>
2026-07-22 18:28:37 +0530

    fix: export default app for Vercel Node serverless function handler

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import app from "../backend/app.js";
 import { connectDB } from "../backend/src/config/db.js";
 
-export default async function handler(req, res) {
+app.use(async (req, res, next) => {
   try {
     await connectDB();
   } catch (err) {
     console.warn("Vercel Serverless DB Connection Warning:", err.message);
   }
-  return app(req, res);
-}
+  next();
+});
+
+export default app;
